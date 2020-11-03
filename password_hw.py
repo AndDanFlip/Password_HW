8592fc2fcf3e37c04e67be3f351fceefdf8b81ff
AndDanFlip <andr33vdanil@yandex.ru>
2020-11-03 20:59:40 +0300

    HomeWork

--- password_hw.py
+++ password_hw.py
@@ -6,12 +6,14 @@ a = input('Введите пароль: ')
 length = len(a)
 
 check = 8
-a = int(a)
 
 try:
     div = check/length
+
+    div = int(div)
+
+    print('Ваш пароль состоит только из цифр')
 except ZeroDivisionError:
     print('Вы ввели пустой пароль')
 except ValueError:
     print('Требования к паролю соблюдены') 
-print('Ваш пароль состоит только из цифр')
